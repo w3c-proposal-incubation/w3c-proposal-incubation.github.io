5d1e3a316ab94e220d20c01c17533bfdbcdd027a
DIYgod <diy.d.god@gmail.com>
2019-07-12 15:05:06 +0800

    fix: pointer-events none to area

--- bulletchatting-proposal/index.js
+++ bulletchatting-proposal/index.js
@@ -80,7 +80,3 @@ video.addEventListener('seeking', () => {
         }
     }
 });
-
-document.querySelector('.area').addEventListener('click', () => {
-    video.paused ? video.play() : video.pause();
-});
